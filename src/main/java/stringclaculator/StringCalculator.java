114e438d8623c0a22bcafaa4c00bd2ab3852df1a
Bellroute <kjkun7631@naver.com>
2018-09-15 02:02:42 +0900

    step0

--- src/main/java/stringclaculator/StringCalculator.java
+++ src/main/java/stringclaculator/StringCalculator.java
@@ -3,6 +3,8 @@ package stringclaculator;
 import java.util.*;
 
 public class StringCalculator {
+    public ArrayList<String> operator;
+    public ArrayList<String> number;
 
 
     public int add(int i, int j) {
@@ -21,251 +23,72 @@ public class StringCalculator {
         return i / j;
     }
 
-    public Queue<String> operation(String input) {
-        String[] splitInput = new String[input.split(" ").length];
-        Queue<String> operator = new Queue<String>() {
-            @Override
-            public boolean add(String s) {
-                return false;
-            }
 
-            @Override
-            public boolean offer(String s) {
-                return false;
-            }
-
-            @Override
-            public String remove() {
-                return null;
-            }
-
-            @Override
-            public String poll() {
-                return null;
-            }
-
-            @Override
-            public String element() {
-                return null;
-            }
-
-            @Override
-            public String peek() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<String> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends String> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
-
-        for (int i = 0; i < input.split(" ").length; i++) {
-            splitInput[i] = input.split(" ")[i];
-        }
-
+    private ArrayList operation(String[] splitInput) {
+        operator = new ArrayList<String>();
         for (int i = 0; i < splitInput.length; i++) {
             if (splitInput[i].equals("+") || splitInput[i].equals("-") || splitInput[i].equals("*") || splitInput[i].equals("/")) {
                 operator.add(splitInput[i]);
             }
         }
-
-            return operator;
+        return operator;
     }
 
-    public Queue<Integer> numbers(String input) {
+
+    private ArrayList numbers(String[] splitInput) {
+        number = new ArrayList<String>();
+        for (int i = 0; i < splitInput.length; i++) {
+            if (!(splitInput[i].equals("+") || splitInput[i].equals("-") || splitInput[i].equals("*") || splitInput[i].equals("/"))) {
+                number.add(splitInput[i]);
+            }
+        }
+        return number;
+    }
+
+    public void calculator() {
+        StringCalculator cal = new StringCalculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("식을 입력 : ");
+        String input = scanner.nextLine();
+
+        cal.answer(input);
+
+    }
+
+    public String[] inputsplit(String input) {
         String[] splitInput = new String[input.split(" ").length];
-        Queue<Integer> number = new Queue<Integer>() {
-            @Override
-            public boolean add(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public boolean offer(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public Integer remove() {
-                return null;
-            }
-
-            @Override
-            public Integer poll() {
-                return null;
-            }
-
-            @Override
-            public Integer element() {
-                return null;
-            }
-
-            @Override
-            public Integer peek() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Integer> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Integer> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
 
         for (int i = 0; i < input.split(" ").length; i++) {
             splitInput[i] = input.split(" ")[i];
         }
 
-        for (int i = 0; i < splitInput.length; i++) {
-            if (!splitInput[i].equals("+") || !splitInput[i].equals("-") || !splitInput[i].equals("*") || !splitInput[i].equals("/")) {
-                number.add(Integer.parseInt(splitInput[i])); // toString 문제인듯...
-            }
-        }
-
-        return number;
+        return splitInput;
     }
 
     public int answer(String input) {
         StringCalculator cal = new StringCalculator();
-        int result = 0;
-        Queue<String> operator = cal.operation(input);
-        Queue<Integer> number = cal.numbers(input);
+        operator = cal.operation(cal.inputsplit(input));
+        number = cal.numbers(cal.inputsplit(input));
+        int result = Integer.parseInt(number.get(0));
 
-        while (number != null) {
-            if (operator.poll().equals("+")) {
-                result = cal.add(result, number.poll());
+        for (int i = 1; i < number.size(); i++) {
+            if (operator.get(i - 1).equals("+")) {
+                result = cal.add(result, Integer.parseInt(number.get(i)));
             }
 
-            if (operator.poll().equals("-")) {
-                result = cal.subtact(result, number.poll());
+            if (operator.get(i - 1).equals("-")) {
+                result = cal.subtact(result, Integer.parseInt(number.get(i)));
             }
 
-            if (operator.poll().equals("*")) {
-                result = cal.multiply(result, number.poll());
+            if (operator.get(i - 1).equals("*")) {
+                result = cal.multiply(result, Integer.parseInt(number.get(i)));
             }
 
-            if (operator.poll().equals("/")) {
-                result = cal.divide(result, number.poll());
+            if (operator.get(i - 1).equals("/")) {
+                result = cal.divide(result, Integer.parseInt(number.get(i)));
             }
         }
-
         return result;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("식을 입력 : ");
-        String input = scanner.nextLine();
     }
 }
