98ec8ca25123411d46fb1a46af815eb761bf2ea6
ChangHwan-Lee <dlckdghks66@naver.com>
2018-09-17 18:45:12 +0900

    refactor : reflect feedback

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,70 +2,75 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    double number1 = 0, number2 = 0, result;
-    String operator;
-    String[] values;
-    int index = 1;
-    boolean error = false;
+    private static final int MINIMUM_VALUE_LENGTH = 3;
 
     public double add(double number1, double number2){ return number1 + number2; }
     public double subtract(double number1, double number2){ return number1 - number2; }
     public double multiply(double number1, double number2){ return number1 * number2; }
     public double divide(double number1, double number2){ return number1 / number2; }
 
-    public void inputValue(){
+    //식을 입력받음
+    public String[] inputExpression() {
         Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        values = value.split(" ");
-        if(values.length < 3 ) {
-            error = true;
-            return;
+        String[] elements = scanner.nextLine().split(" ");
+        if(elements.length < MINIMUM_VALUE_LENGTH) {
+            throw new InputException();
         }
-        result = Double.parseDouble(values[0]);
+        return elements;
     }
 
-    public void allocateValue(){
-        number1 = result;
-        operator = values[index];
-        number2 = Double.parseDouble(values[index+1]);
-
-    }
-
-    public void operation(){
+    //연산자와 피연산자를 인수로 받아 한 번의 계산을 수행
+    public double operate(Operand operand, String operator) {
         switch (operator) {
             case "+":
-                result = add(number1, number2);
-                break;
+                return add(operand.number1, operand.number2);
             case "-":
-                result = subtract(number1, number2);
-                break;
+                return subtract(operand.number1, operand.number2);
             case "*":
-                result = multiply(number1, number2);
-                break;
+                return multiply(operand.number1, operand.number2);
             case "/":
-                result = divide(number1, number2);
-                break;
+                return divide(operand.number1, operand.number2);
             default:
-                error = true;
-                break;
+                throw new InputException();
         }
     }
 
-    public double calculation(){
-        inputValue();
-        while(index + 1 < values.length) {
-            allocateValue();
-            index += 2;
-            operation();
-        }
-        if(error == true) {
-            System.out.println("올바르지 않은 입력입니다");
-            return 0;
-        }
-        return result;
-    }
-
+    //메인
     public static void main(String[] args) {
-        System.out.println(new Calculator().calculation());
+        double number1, number2, temp, result;
+        String operator;
+        Operand operand;
+        Calculator calculator = new Calculator();
+        String[] elements = calculator.inputExpression();
+
+        //최초에 입력받은 식에서 한 번의 계산을 반복
+        temp = Double.parseDouble(elements[0]);
+        for (int index = 1; index + 1 < elements.length; index += 2) {
+            number1 = temp;
+            operator = elements[index];
+            number2 = Double.parseDouble(elements[index+1]);
+            operand = new Operand(number1, number2);
+            temp = calculator.operate(operand, operator);
+        }
+
+        result = temp;
+        System.out.println(result);
+    }
+}
+
+//피연산자 두개를 가지는 클래스
+class Operand {
+    double number1, number2;
+
+    Operand(double number1, double number2){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+}
+
+//올바르지 않은 입력에 대한 사용자 정의 예외
+class InputException extends RuntimeException{
+    InputException(){
+        super("올바르지 않은 입력입니다.");
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,7 @@
-import org.junit.After;
-import org.junit.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     Calculator cal;
@@ -13,27 +13,22 @@ public class CalculatorTest {
 
     @Test
     public void 덧셈() {
-        Assert.assertEquals(7, cal.add(3, 4), 0.0);
+        assertEquals(7, cal.add(3, 4), 0.0);
     }
 
     @Test
     public void 뺄셈() {
-        Assert.assertEquals(1, cal.subtract(5, 4), 0.0);
+        assertEquals(1, cal.subtract(5, 4), 0.0);
     }
 
     @Test
     public void 곱셈() {
-        Assert.assertEquals(6, cal.multiply(2, 3), 0.0);
+        assertEquals(6, cal.multiply(2, 3), 0.0);
     }
 
     @Test
     public void 나눗셈() {
-        Assert.assertEquals(2, cal.divide(8, 4), 0.0);
+        assertEquals(2, cal.divide(8, 4), 0.0);
     }
 
-
-    @After
-    public void tearDown() throws Exception {
-        cal = null;
-    }
 }
