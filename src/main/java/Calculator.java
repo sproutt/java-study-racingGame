4d16dfcb2bb51812299c576b3ed55deace877351
dadadamarine <dadadamarine@naver.com>
2019-01-11 01:22:52 +0900

    fix : 2차 리뷰 반영

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static final int infinite = 999999;
+    private static final int INFINITE = 999999;
 
     public int add(int i, int j) {
         return i + j;
@@ -21,8 +21,7 @@ public class Calculator {
 
     public String getValue() {
         Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        return value;
+        return scanner.nextLine();
     }
 
     public String[] splitString(String value){
@@ -31,29 +30,36 @@ public class Calculator {
     }
 
     public int calculate(int before, String operator, int after) {
-        int result = infinite;
-        if (operator.equals("+")) result = add(before, after);
-        if (operator.equals("-")) result = subtract(before, after);
-        if (operator.equals("/") || operator.equals("%")) result = divide(before, after);
-        if (operator.equals("*")) result = multiple(before, after);
+        int result = INFINITE;
+        if (operator.equals("+")){
+            result = add(before, after);
+        }
+        if (operator.equals("-")){
+            result = subtract(before, after);
+        }
+        if (operator.equals("/") || operator.equals("%")){
+            result = divide(before, after);
+        }
+        if (operator.equals("*")){
+            result = multiple(before, after);
+        }
         return result;
     }
 
-    public int devideString(String[] inputArr) {
-        //System.out.println(before);
-        int result = toInt(inputArr[0]);
-        for(int i =0; i<inputArr.length-2; i+=2){
-            result = calculate(result, inputArr[i+1], toInt(inputArr[i+2]));
+    public int devideString(String[] inputArray) {
+        int result = convertStringToInt(inputArray[0]);
+        for(int i =0; i<inputArray.length-2; i+=2){
+            result = calculate(result, inputArray[i+1], convertStringToInt(inputArray[i+2]));
         }
         return result;
     }
 
     public void print(int result) {
-        if (result == infinite) System.out.println("잘못 입력하셨습니다");
-        if (result != infinite) System.out.println("결과 : " + result);
+        if (result == INFINITE) System.out.println("잘못 입력하셨습니다");
+        if (result != INFINITE) System.out.println("결과 : " + result);
     }
 
-    public int toInt(String str){
-        return Integer.parseInt(str);
+    public int convertStringToInt(String number){
+        return Integer.parseInt(number);
     }
 }
