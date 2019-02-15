db6e53d49d3bb7e4582746bddfd587d9c92f5146
dadadamarine <dadadamarine@naver.com>
2019-02-15 17:21:53 +0900

    fix : 로컬 마스터 브랜치 삭제 및 ms브랜치 생성

--- src/main/java/racingGame/view/InputView.java
+++ src/main/java/racingGame/view/InputView.java
@@ -9,11 +9,13 @@ public class InputView {
     public static String[] getCarsNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String[] carNames = scanner.nextLine().split(",");
+
         return carNames;
     }
 
     public static int getTryNumber() {
         System.out.println("시도할 회수는 몇 회 인가요?");
+
         return scanner.nextInt();
     }
 }
