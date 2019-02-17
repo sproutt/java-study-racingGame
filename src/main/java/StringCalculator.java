43353521efae084a7711f8620aa9a89fd9c15bf0
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-02-18 02:55:14 +0900

    fix : var name && feat : some function

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,60 +1,70 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StringCalculator {
+    public static int calculateOperandCount(int[] array){
+        int operandCount = 0 ;
+        for(int i = 0 ; i < array.length ; i++ ){
+            if(!(array[i]>=48 && array[i]<=57)){
+                operandCount++;
+            }
+        }
+        return operandCount;
+    }
+    public static int calculateNumberCount(int operandCount){
+        return operandCount+1;
+    }
+    public static int makeAnswer(int[] numberArray,int[] operandArray){
+        int tempAnswer = numberArray[0];
+        for(int i = 0 ; i < operandArray.length ; i++){
+            if(operandArray[i] == '+'){
+                tempAnswer += numberArray[i+1];
+            }
+            else if(operandArray[i] == '-'){
+                tempAnswer -= numberArray[i+1];
+            }
+            else if(operandArray[i] == '*'){
+                tempAnswer *= numberArray[i+1];
+            }
+            else if(operandArray[i] == '/') {
+                tempAnswer /= numberArray[i + 1];
+            }
+        }
+        return tempAnswer;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        value = value.replace(" ","");
+        String originString = scanner.nextLine();
+        originString = originString.replace(" ","");
+        int[] array = new int[originString.length()];
+        for(int i = 0 ; i < originString.length() ; i++){
+            array[i] = originString.charAt(i);
+        }
 
-        ArrayList<String> numberList = new ArrayList<>();
-        ArrayList<String> logicList = new ArrayList<>();
-
-        String num = "";
-        char c1, c2;
-
-        for(int i =  0  ; i < value.length() -1 ; i++){
-            c1 = value.charAt(i);
-            c2 = value.charAt(i+1);
-
-            if(Character.isDigit(c1)){
-                num += c1;
-                if(!Character.isDigit(c2)){
-                    numberList.add(num);
-                    num="";
+        int[] numberArray = new int[calculateNumberCount(calculateOperandCount(array))];
+        int[] operandArray = new int[calculateOperandCount(array)];
+        int numberArrayIndex = 0;
+        int operandArrayIndex = 0;
+        int tempNum = 0;
+        //
+        for(int i = 0 ; i < array.length ; i++){
+            if(i ==array.length-1){
+                tempNum *= 10;
+                numberArray[numberArrayIndex] = tempNum+array[i]-48;
+            }
+            else{
+                if(array[i]>=48 && array[i]<=57){
+                    tempNum *= 10;
+                    tempNum += array[i]-48;
+                }
+                else{
+                    operandArray[operandArrayIndex] = array[i];
+                    numberArray[numberArrayIndex] = tempNum;
+                    tempNum=0;
+                    numberArrayIndex++;
+                    operandArrayIndex++;
                 }
             }
-            if(!Character.isDigit(c1)){
-                logicList.add(String.valueOf(c1));
-                if( i == value.length() -2){
-                    numberList.add(String.valueOf(c2));
-                }
-            }
         }
-        int[] numArray = new int[numberList.size()];
-        for(int i = 0 ; i < numberList.size() ; i++){
-            numArray[i] = Integer.parseInt(numberList.get(i));
-        }
-        for(int i = 0 ; i < logicList.size() ; i++){
-            System.out.println(logicList.get(i));
-        }
-
-        int prev = numArray[0];
-        for(int i = 0 ; i < logicList.size() ; i++){
-            if(logicList.get(i) == "+"){
-                prev = prev + numArray[i+1];
-            }
-            else if(logicList.get(i) == "-"){
-                prev = prev - numArray[i+1];
-            }
-            else if(logicList.get(i) == "*"){
-                prev = prev * numArray[i+1];
-            }
-            else if (logicList.get(i) == "/"){
-                prev = prev / numArray[i+1];
-            }
-       }
-        System.out.println(prev);
+        System.out.println(makeAnswer(numberArray,operandArray));
     }
 }
