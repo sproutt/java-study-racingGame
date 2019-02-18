b971e6b8aac7a9d7dec9ff419c839c89c12298f3
unknown <a01054394111@gmail.com>
2019-02-18 17:24:53 +0900

    add : CalculatorTest.java and setUp

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+    public void setUp(){
+        calculator = new Calculator();
+    }
+}
