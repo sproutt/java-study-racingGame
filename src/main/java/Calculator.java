bde7e5b669c56b4f7cb826c6b818f01996b10040
unknown <a01054394111@gmail.com>
2019-02-18 17:11:27 +0900

    feat : plot inputString

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,15 @@
 import java.util.regex.*;
 public class Calculator {
-    
+    private String[] plotInputNumber(String inputString){
+        inputString = inputString.replaceAll(" ","");
+        Pattern operatorPattern = Pattern.compile("[+*/-]");
+        String[] plotedNumber = operatorPattern.split(inputString);
+        return plotedNumber;
+    }
+
+    private String plotInputOperator(String inputString){
+        inputString = inputString.replaceAll(" ","");
+        String plotedOperator = inputString.replaceAll("[0-9]+","");
+        return plotedOperator;
+    }
 }
