c4eaa7146eb609b07f3ec9f9f97542a4713a9320
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-02-20 05:59:35 +0900

    feat : 4칙연산 테스트

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,9 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+    @Before
+    public void setUp(){
+        stringCalculator = new StringCalculator();
+    }
     @Test
-    public static int add(int a, int b) {
-     a+=b;
-     return a;
+    public void 덧셈( ) {
+        assertEquals(7 , stringCalculator.add(3,4));
+    }
+    @Test
+    public void 뺄셈() {
+        assertEquals(2, stringCalculator.minus(5,3));
+    }
+    @Test
+    public void 곱셈(){
+        assertEquals(12, stringCalculator.multiple(3,4));
+    }
+    @Test
+    public void 나눗셈(){
+        assertEquals(3,stringCalculator.divide(15,5));
+    }
+    @Test
+    public void 연산자갯수확인(){
+        //assertEquals(3,stringCalculator.operandCheck(0, new String[]{"+", "-", "/"},);
     }
 }
