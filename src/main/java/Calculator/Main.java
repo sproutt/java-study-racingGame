84af22a6217f55651a6605e59277ed4f7a58a6a1
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-02-27 21:07:04 +0900

    reflect feedback

---
+++ src/main/java/Calculator/Main.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator;
+        calculator = new Calculator();
+
+        System.out.println(calculator.inputString());
+    }
+}
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,5 @@
+import Calculator.Calculator;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,49 +7,32 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
-    StringCalculator stringCalculator;
+    Calculator calculator;
 
     @Before
     public void setUp() {
-        stringCalculator = new StringCalculator();
+        calculator = new Calculator();
     }
-
     @Test
     public void 덧셈() {
-        assertEquals(7, stringCalculator.add(3, 4));
+        assertEquals(7, calculator.add(3, 4));
     }
-
     @Test
     public void 뺄셈() {
-        assertEquals(2, stringCalculator.minus(5, 3));
+        assertEquals(2, calculator.minus(5, 3));
     }
 
     @Test
     public void 곱셈() {
-        assertEquals(12, stringCalculator.multiple(3, 4));
+        assertEquals(12, calculator.multiple(3, 4));
     }
-
     @Test
     public void 나눗셈() {
-        assertEquals(3, stringCalculator.divide(15, 5));
+        assertEquals(3, calculator.divide(15, 5));
     }
 
-    @Test
-    public void 연산자확인() {
-        assertEquals(1, stringCalculator.operands(0, '+'));
-        assertEquals(2, stringCalculator.operands(1, '-'));
-        assertEquals(1, stringCalculator.operands(0, '*'));
-        assertEquals(3, stringCalculator.operands(2, '/'));
-    }
-    @Test
-    public void 연산자갯수세기() {
-        int[] array = {'2','+','3','*','4','/','2'};
-        assertEquals(3,StringCalculator.calculateOperandCount(array));
-    }
-    @Test
-    public void 정답(){
-        int[] numverArray = {2,3,4,2};
-        int[] operandArray = {'+','*','/'};
-        assertEquals(10,StringCalculator.makeAnswer(numverArray,operandArray));
+    @After
+    public void tearDown(){
+        calculator = null;
     }
 }
