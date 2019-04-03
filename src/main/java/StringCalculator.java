35b5acbba1985474dfe0cca177e66931ed24969c
jbj616 <jbj616@github.com>
2019-04-04 00:36:21 +0900

    StringCalculator 다시 구현

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class StringCalculator {
@@ -18,73 +16,26 @@ public class StringCalculator {
     public int makeResult(String input) {
         if (isBlank(input))
             throw new RuntimeException();
-        return caculateQueue(operandQueue(splitBlank(input)), operatorQueue(splitBlank(input)));
+        return calculateSplitedString(splitBlank(input));
     }
 
     public String[] splitBlank(String str) {
         return str.split(" ");
     }
 
-    public Queue<Integer> operandQueue(String[] separatedString) {
-        Queue<Integer> operandQueue = new LinkedList<>();
-        for (int i = 0; i < separatedString.length; i++) {
-            operandQueue = addOperandQueue(operandQueue, separatedString[i]);
-        }
-        return operandQueue;
-    }
-
-    public Queue<Character> operatorQueue(String[] separatedString) {
-        Queue<Character> operatorQueue = new LinkedList<>();
-        for (int i = 0; i < separatedString.length; i++) {
-            operatorQueue = addOperatorQueue(operatorQueue, separatedString[i]);
-        }
-        return operatorQueue;
-    }
-
-    public Queue<Integer> addOperandQueue(Queue<Integer> operandQueue, String str) {
-        if (isOperand(str)) {
-            operandQueue.add(toInt(str));
-        }
-        return operandQueue;
-    }
-
-    public Queue<Character> addOperatorQueue(Queue<Character> operatorQueue, String str) {
-        if (isOperator(str)) {
-            operatorQueue.add(str.charAt(0));
-        }
-        return operatorQueue;
-    }
-
-    public boolean isOperand(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (!('0' <= str.charAt(i) && str.charAt(i) < '9')) return false;
-        }
-        return true;
-    }
-
-    public boolean isOperator(String str) {
-        if (str.length() != 1) {
-            return false;
-        }
-        if (!(str.charAt(0) == '+' || str.charAt(0) == '-' || str.charAt(0) == '/' || str.charAt(0) == '*')) {
-            return false;
-        }
-        return true;
-    }
-
     public int toInt(String str) {
         return Integer.parseInt(str);
     }
 
-    public int caculateQueue(Queue<Integer> operand, Queue<Character> operator) {
-        int result = operand.poll();
-        while (!operand.isEmpty()) {
-            result = calculate(result, operator.poll(), operand.poll());
+    public int calculateSplitedString(String[] str) {
+        int result = toInt(str[0]);
+        for (int i = 0; i < str.length - 2; i += 2) {
+            result = calculate(result, str[i + 1].charAt(0), toInt(str[i + 2]));
         }
         return result;
     }
 
-    public int calculate(int firstValue, int operator, int secondValue) {
+    public int calculate(int firstValue, char operator, int secondValue) {
         if (operator == '+')
             return add(firstValue, secondValue);
         if (operator == '-')
