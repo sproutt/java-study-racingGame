c8c4967b3509ac7a54e5e91789d46fc1257de3b9
jbj616 <jbj616@github.com>
2019-04-07 21:02:00 +0900

    rebase

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     }
 
     public static int InputTime() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println("시도할 횟수는 몇 회 인가요?");
         return new Scanner(System.in).nextInt();
     }
 }
